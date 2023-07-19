eab9e1a75e1abb46091fc21f1c3f8800c73d1335
Guilherme Aguiar <guilherme.aguiar@prodigasistemas.com.br>
2023-07-19 09:09:39 -0300

    Teste metodo de login

--- lib/cliente_api/integracao/autenticacao.rb
+++ lib/cliente_api/integracao/autenticacao.rb
@@ -16,7 +16,7 @@ module ClienteAPI
       end
 
       def autenticar_usuario(params)
-        response = RestClient.post "#{ClienteAPI::Base::URL_BASE}/autenticacao", params, Requisicao.basic_auth
+        response = RestClient.post "#{ClienteAPI::Base::URL_BASE}/autenticacao", params, basic_auth
         json = JSON.parse(response.body)
 
         json["usuario"]
